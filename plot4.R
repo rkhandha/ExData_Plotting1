7baf20aac0e44698f5533ca4ef54de895a0d52df
Rekha <rkhandha@hotmail.com>
2014-06-07 20:56:18 -0400

    Modified plot4 margins and fonts

--- plot4.R
+++ plot4.R
@@ -16,22 +16,22 @@ plot4 <- function () {
   
     par(mfrow = c(2,2))
   #set graph alignments on the page
-    par(mar = c(3,3,2,1))
+    par(mar = c(2,3,2,1))
   # set lable gap
     par(mgp = c(1.5,.5,0))
   # plot line graph Global_active_power
-    plot(yrange, xglobalactivepower, type="l",  ylab='Global Active Power (killowatts)',xlab="", cex.lab= 0.8) 
+    plot(yrange, xglobalactivepower, type="l",  ylab='Global Active Power',xlab="", cex.lab= 0.8) 
   
   # plot line graph for Voltage
-    plot(yrange, xvoltage, type="l",  ylab='Voltage', xlab='datetime') 
+    plot(yrange, xvoltage, type="l",  ylab='Voltage', xlab='datetime',cex.lab= 0.8) 
   
   # plot line graph for Submetering variables 
-    plot(yrange, subrt$Sub_metering_1, type="l",  ylab='Energy Submetering', xlab="") 
+    plot(yrange, subrt$Sub_metering_1, type="l",  ylab='Energy Submetering', xlab="", cex.lab= 0.8) 
     lines(yrange,subrt$Sub_metering_2,col="red")
     lines(yrange,subrt$Sub_metering_3,col="blue")
-    legend("topright", c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), cex=0.3, col=c('black','red','blue'), lwd=2)
+    legend("topright", c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), cex=0.6, col=c('black','red','blue'), lwd=2,bty="n")
   
-    plot(yrange, xglobalreactivepower, type="l",  ylab='Global_reactive_power', xlab='datetime') 
+    plot(yrange, xglobalreactivepower, type="l",  ylab='Global_reactive_power', xlab='datetime',cex.lab= 0.8) 
     
   # create plot1.png file
     dev.copy(png, file = "plot4.png")
